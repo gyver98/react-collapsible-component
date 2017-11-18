423a8a5c5af92ff1808d630c6fc3ca61cdb7ed85
somdari <matthewchoi7@gmail.com>
2017-11-18 14:34:46 +1100

    adding Collapsible comp

---
+++ src/js/Collapsible.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Collapsible extends React.Component {
+  render() {
+    return (
+      <p>text</p>
+    )
+  }
+}
+
+export default Collapsible;
