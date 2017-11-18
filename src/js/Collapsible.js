c906cd811cbd245180df18f6ce084da233bfff3f
somdari <matthewchoi7@gmail.com>
2017-11-18 15:42:07 +1100

    adding Collapsible comp

--- src/js/Collapsible.js
+++ src/js/Collapsible.js
@@ -5,12 +5,23 @@ class Collapsible extends React.Component {
 
   constructor(props) {
     super(props)
+    this.state = {
+      isExpanded: false
+    }
+  }
+
+  handleToggle(e) {
+    e.preventDefault();
+    this.setState({
+      isExpanded: !this.state.isExpanded
+    })
   }
 
   render() {
     const { title, children } = this.props;
+    const { isExpanded } = this.state;
     return (
-      <div className="panel">
+      <div className={`panel ${isExpanded ? 'is-expanded':''}`} onClick={(e) => this.handleToggle(e)}>
         <div className="panel-heading">
           <h2>{title}</h2>
         </div>
