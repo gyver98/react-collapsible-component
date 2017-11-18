c401780f90f671d0e95bf9df565ea4259eb7f4c9
somdari <matthewchoi7@gmail.com>
2017-11-18 15:11:01 +1100

    adding Collapsible comp

--- src/js/App.js
+++ src/js/App.js
@@ -3,35 +3,22 @@ import image from '../images/expand-vertical-4.svg';
 import Collapsible from './Collapsible';
 
 class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <header>
-                    <img src={image} />
-                    <h1>Collapsible Content</h1>
-                </header>
-                <div className="content">
-                    <div className="panel-group">
-                        <Collapsible title="Overview">
-                            <p>
-                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.  
-                            </p>
-                        </Collapsible>
-                        
-                        <div className="panel">
-                            <div className="panel-heading">
-                                <h2>Reviews</h2>
-                            </div>
-                            <div className="panel-collapse">
-                                <div className="panel-body">
-                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <header>
+          <img src={image} />
+          <h1>Collapsible Content</h1>
+        </header>
+        <div className="content">
+          <div className="panel-group">
+            <Collapsible title="Overview">
+              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute.
+            </Collapsible>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default App;
--- src/js/Collapsible.js
+++ src/js/Collapsible.js
@@ -2,9 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Collapsible extends React.Component {
+
+  constructor(props) {
+    super(props)
+  }
+
   render() {
+    const { title, children } = this.props;
     return (
-      <p>text</p>
+      <div className="panel">
+        <div className="panel-heading">
+          <h2>{title}</h2>
+        </div>
+        <div className="panel-collapse">
+          <div className="panel-body">
+            {children}
+          </div>
+        </div>
+      </div>
     )
   }
 }
